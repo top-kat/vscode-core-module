8976853d5ff98633f4bdd3c13d252d61a75151fb
TOPKAT <webmaster@nicewebagence.com>
2022-12-01 10:38:28 +0100

    add synopsis auto on test

--- src/generate.js
+++ src/generate.js
@@ -1,43 +1,43 @@
-const vscode = require('vscode');
-const window = vscode.window;
-const Path = require('path');
+const vscode = require('vscode')
+const window = vscode.window
+const Path = require('path')
 const fs = require('fs')
-const templater = require('simple-file-templater');
+const templater = require('simple-file-templater')
 
 
-const { isset, randomItemInArray, firstMatch, camelCase, pascalCase, dashCase, asArray } = require('../utils');
+const { isset, randomItemInArray, firstMatch, camelCase, pascalCase, dashCase, asArray } = require('../utils')
 
 // let serverBasePath;
 // let frontBasePath;
 module.exports = () => {
     vscode.commands.registerCommand('coreVscodeModule.generate', async () => {
         try {
-            let basePath, isFront, isBack, isOneLevelAboveRoot = false;
+            let basePath, isFront, isBack, isOneLevelAboveRoot = false
 
-            const workspacePath = vscode.workspace.workspaceFolders[0].uri.path;
+            const workspacePath = vscode.workspace.workspaceFolders[0].uri.path
 
             if (workspacePath.includes('/server')) {
-                isFront = false;
-                isBack = true;
-                basePath = Path.join(workspacePath, 'src');
+                isFront = false
+                isBack = true
+                basePath = Path.join(workspacePath, 'src')
             } else if (workspacePath.includes('front')) {
-                isFront = true;
-                isBack = false;
-                basePath = Path.join(workspacePath, 'src');
+                isFront = true
+                isBack = false
+                basePath = Path.join(workspacePath, 'src')
             } else {
                 const backOrFront = await Q({
                     prompt: randomItemInArray([`Hi DAD!! What do you want I generate for you today ?`, `Huh O_o !! You waked me up!`, `Lazy boy! Do you really need that I do the work for you ?`, 'Oh SHIT! let me finish first!']),
                     choices: [`BACK`, `FRONT`],
-                });
-                isFront = backOrFront === 'FRONT';
-                isBack = backOrFront === 'BACK';
-                const folderForEnd = isFront ? 'front' : 'back';
-                console.log(`workspacePath`, workspacePath);
-                if (fs.existsSync(Path.join(workspacePath, 'src'))) basePath = Path.join(workspacePath, 'src');
+                })
+                isFront = backOrFront === 'FRONT'
+                isBack = backOrFront === 'BACK'
+                const folderForEnd = isFront ? 'front' : 'back'
+                console.log(`workspacePath`, workspacePath)
+                if (fs.existsSync(Path.join(workspacePath, 'src'))) basePath = Path.join(workspacePath, 'src')
                 else {
                     basePath = (await vscode.workspace.findFiles(`**/${folderForEnd}/src/**/*`)).reduce((serverBasePath, file) => {
-                        return serverBasePath || file.path.split(`/${folderForEnd}/src/`)[0] + `/${folderForEnd}/src/`;
-                    }, false);
+                        return serverBasePath || file.path.split(`/${folderForEnd}/src/`)[0] + `/${folderForEnd}/src/`
+                    }, false)
                     isOneLevelAboveRoot = true
                 }
             }
@@ -58,7 +58,7 @@ module.exports = () => {
                     'COMPONENT',
                     `MODULE`,
                 ],
-            });
+            })
 
             if (whatToGenerate === `MODULE`) {
                 //----------------------------------------
@@ -67,36 +67,36 @@ module.exports = () => {
                 const moduleName = await Q({
                     prompt: `Module name ?`,
                     validateInput: str => isset(str) && str.length ? null : 'Cannot be empty',
-                });
+                })
                 const varz = {
                     ...moduleNameVarz(moduleName),
                     // others
                     '/\\*eslint-disable\\*/': '',
                     '// here for intellisense': '',
-                };
+                }
 
                 const createdPaths = templater.templater(
                     Path.join(basePath, `${corePathRoot}/templates/new-module`), // from
                     Path.join(basePath, isFront ? varz.MyNewModule : varz['my-new-module']), // to
                     varz, // replace
                     { module: varz['my-new-module'], Module: varz.MyNewModule, }, // file name replacer
-                );
+                )
 
-                await openFiles(...createdPaths.filter(p => !p.includes('error')));
+                await openFiles(...createdPaths.filter(p => !p.includes('error')))
             } else {
-                const frontModuleNames = await findAllModuleNames(isOneLevelAboveRoot ? 'frontend/src' : 'src', [corePathRoot, '/dist/']);
-                const backModuleNames = await findAllModuleNames(isOneLevelAboveRoot ? 'server/src' : 'src', [corePathRoot, '/dist/']);
+                const frontModuleNames = await findAllModuleNames(isOneLevelAboveRoot ? 'frontend/src' : 'src', [corePathRoot, '/dist/'])
+                const backModuleNames = await findAllModuleNames(isOneLevelAboveRoot ? 'server/src' : 'src', [corePathRoot, '/dist/'])
 
                 const selectedModuleR = await Q({
                     prompt: `In which module?`,
                     choices: isFront ? frontModuleNames : backModuleNames,
                     allowCustomValues: true,
-                });
-                const selectedModule = selectedModuleR.toString(); // hack for no red to appear
+                })
+                const selectedModule = selectedModuleR.toString() // hack for no red to appear
                 const fileName = await Q({
                     prompt: whatToGenerate === `COMPONENT` ? `Component name ?` : `File name without type extension (Eg: 'user-update' OR 'reservation-cancel')`,
                     validateInput: str => isset(str) && str.length ? null : 'Cannot be empty',
-                });
+                })
 
 
                 if ([`SERVICE`, `IMPORT`, `DEFINITIONS`, `MODEL`, `DAO`, `SEED`, `USERFLOWS`].includes(whatToGenerate)) {
@@ -114,9 +114,9 @@ module.exports = () => {
                         userflows: [`.user-flow.ts`, `module.user-flow.ts`, ``],
                     }
                     const [extension, templateName, folderName] = extensions[whatToGenerate.toLowerCase()]
-                    const generatedFilePath = Path.join(basePath, selectedModule, folderName, fileName + extension);
-                    const templatePath = Path.join(basePath, `${corePathRoot}/templates/${templateName}`);
-                    await writeAndopenFile([generatedFilePath, templatePath, moduleNameVarz(selectedModule)]);
+                    const generatedFilePath = Path.join(basePath, selectedModule, folderName, fileName + extension)
+                    const templatePath = Path.join(basePath, `${corePathRoot}/templates/${templateName}`)
+                    await writeAndopenFile([generatedFilePath, templatePath, moduleNameVarz(selectedModule)])
                 } else if (whatToGenerate === `COMPONENT`) {
                     //----------------------------------------
                     // COMPONENT
@@ -126,48 +126,48 @@ module.exports = () => {
                         // others
                         '/\\*eslint-disable\\*/': '',
                         '// here for intellisense': '',
-                    };
+                    }
 
                     const createdPaths = templater.templater(
                         Path.join(basePath, `${corePathRoot}/templates/new-component`), // from
                         Path.join(basePath, selectedModule, `${varz.MyNewModule}`), // to
                         varz, // replace
                         { module: varz['my-new-module'], Module: varz.MyNewModule }, // file name replacer
-                    );
+                    )
 
-                    await openFiles(...createdPaths);
-                } else vscode.window.showInformationMessage(randomItemInArray(['Next time avoid waking me up', 'What! All that for.......that ?!', 'little dick', `You don't dare ? Dare you ?`, `Next time I'll stay in my bed!`]));
+                    await openFiles(...createdPaths)
+                } else vscode.window.showInformationMessage(randomItemInArray(['Next time avoid waking me up', 'What! All that for.......that ?!', 'little dick', `You don't dare ? Dare you ?`, `Next time I'll stay in my bed!`]))
             }
         } catch (err) {
-            console.error(`err`, err);
-            vscode.window.showErrorMessage(err.toString());
-            vscode.window.showInformationMessage(randomItemInArray(['Whooops!! shit happens...', 'What! All that for.......that ?!', 'little dick', `You don't dare ? Dare you ?`, `Next time I'll stay in my bed!`]));
+            console.error(`err`, err)
+            vscode.window.showErrorMessage(err.toString())
+            vscode.window.showInformationMessage(randomItemInArray(['Whooops!! shit happens...', 'What! All that for.......that ?!', 'little dick', `You don't dare ? Dare you ?`, `Next time I'll stay in my bed!`]))
         }
-    });
-};
+    })
+}
 
 //----------------------------------------
 // HELPERS
 //----------------------------------------
 
 function moduleNameVarz(moduleName) {
-    if (/[A-Z]/.test(moduleName)) moduleName = moduleName.replace(/([A-Z])/g, '-$1'); // allow camelCase
+    if (/[A-Z]/.test(moduleName)) moduleName = moduleName.replace(/([A-Z])/g, '-$1') // allow camelCase
 
-    const moduleNameBits = moduleName.toLowerCase().split('-');
+    const moduleNameBits = moduleName.toLowerCase().split('-')
     return {
         myNewModule: camelCase(...moduleNameBits),
         MyNewModule: pascalCase(...moduleNameBits),
         'my-new-module': dashCase(...moduleNameBits),
-    };
+    }
 }
 
 
 async function Q(questionConfig) {
     if (questionConfig.choices) {
-        const { choices, allowCustomValues } = questionConfig;
+        const { choices, allowCustomValues } = questionConfig
         return new Promise((resolve) => {
-            const quickPick = window.createQuickPick();
-            quickPick.items = choices.map(choice => ({ label: choice }));
+            const quickPick = window.createQuickPick()
+            quickPick.items = choices.map(choice => ({ label: choice }))
             //  if (questionConfig.prompt) quickPick.placeholder = questionConfig.prompt
             if (questionConfig.prompt) quickPick.title = questionConfig.prompt
             if (allowCustomValues) {
@@ -184,14 +184,14 @@ async function Q(questionConfig) {
                 resolve(selection.label)
                 quickPick.hide()
             })
-            quickPick.show();
+            quickPick.show()
         })
     } else {
-        return await window.showInputBox(questionConfig);
+        return await window.showInputBox(questionConfig)
     }
 }
 async function openFiles(...filePaths) {
-    for (const filePath of asArray(filePaths)) await vscode.workspace.openTextDocument(vscode.Uri.file(filePath)).then(doc => vscode.window.showTextDocument(doc, { preview: false }));
+    for (const filePath of asArray(filePaths)) await vscode.workspace.openTextDocument(vscode.Uri.file(filePath)).then(doc => vscode.window.showTextDocument(doc, { preview: false }))
 }
 /**
  * @param  {...any} configs [filePath, templatePath]
@@ -203,25 +203,25 @@ async function writeAndopenFile(...configs) {
             filePath, // to
             varz, // replace
             replaceInFileNames, // file name replacer
-        );
+        )
         // await fs.outputFile(filePath, fs.readFileSync(templatePath, 'utf-8'));
-        await openFiles(filePath);
+        await openFiles(filePath)
     }
 }
 
 async function findAllModuleNames(nameOfRootFolder, ignorePatterns) {
-    const allServerModuleConfigFilesUri = await vscode.workspace.findFiles(`**/${nameOfRootFolder}/**/*`);
+    const allServerModuleConfigFilesUri = await vscode.workspace.findFiles(`**/${nameOfRootFolder}/**/*`)
     const allModulePaths = allServerModuleConfigFilesUri.reduce((uniqueModulePaths, actualFullPath) => {
-        const [, modulePath] = actualFullPath.path.match(new RegExp(`(/${nameOfRootFolder}/[^/]+/).*$`)) || [];
-        if (modulePath && !uniqueModulePaths.includes(modulePath) && ignorePatterns.every(ignorePattern => !modulePath.includes(ignorePattern))) uniqueModulePaths.push(modulePath);
-        return uniqueModulePaths;
-    }, []);
+        const [, modulePath] = actualFullPath.path.match(new RegExp(`(/${nameOfRootFolder}/[^/]+/).*$`)) || []
+        if (modulePath && !uniqueModulePaths.includes(modulePath) && ignorePatterns.every(ignorePattern => !modulePath.includes(ignorePattern))) uniqueModulePaths.push(modulePath)
+        return uniqueModulePaths
+    }, [])
 
     const allModules = allModulePaths.reduce((moduleNames, actualPth) => {
-        const moduleName = firstMatch(actualPth, new RegExp(`${nameOfRootFolder}.([^\\/]+).`));
-        if (!moduleNames.includes(moduleName)) return [...moduleNames, moduleName];
-        else return moduleNames;
-    }, []);
-    allModules.sort();
-    return allModules;
+        const moduleName = firstMatch(actualPth, new RegExp(`${nameOfRootFolder}.([^\\/]+).`))
+        if (!moduleNames.includes(moduleName)) return [...moduleNames, moduleName]
+        else return moduleNames
+    }, [])
+    allModules.sort()
+    return allModules
 }
--- src/highlight.js
+++ src/highlight.js
@@ -137,3 +137,31 @@ function regexpHighlightFirstCapturingGroup(editor, regexp, styleForCapturingGro
         editor.setDecorations(style, rangesLocal)
     })
 }
+
+//----------------------------------------
+// TEST FLOW AUTO SYNOPSIS
+//----------------------------------------
+workspace.onWillSaveTextDocument(async function (event) {
+    console.log(`HIGH`)
+    try {
+        const editor = window.activeTextEditor
+        if (editor && event.document === editor.document && editor.document.fileName.endsWith('test-flow.ts')) {
+            const text = editor.document.getText()
+            const match = /^\/\* SYNOPSIS[\s\S]*?\*\/\n?\n?/.exec(text) || []
+            const alreadyExistingSelection = isset(match[0])
+            const allDescriptionMatches = allMatches(text, /doc: `(.+?)`,\s*\n/g)
+            let synopsis = '/* SYNOPSIS\n\n'
+            let i = 0
+            for (const [, description] of allDescriptionMatches) synopsis += `${++i}) ${description}\n`
+            synopsis += '*/\n\n'
+            await editor.edit(editBuilder => {
+                if (alreadyExistingSelection) {
+                    const deleteStart = editor.document.positionAt(match.index)
+                    const deleteEnd = editor.document.positionAt(match.index + match[0].length)
+                    editBuilder.delete(new vscode.Range(deleteStart, deleteEnd))
+                }
+                editBuilder.insert(editor.document.positionAt(0), synopsis)
+            })
+        }
+    } catch (err) { console.error(err) }
+}, null, ctx.subscriptions)
